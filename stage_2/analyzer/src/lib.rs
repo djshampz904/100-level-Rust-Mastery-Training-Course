d7cff6e0abcbe49f642f3546d68fb4da46b95082
djshampz904 <martin.kimani@strathmore.edu>
2025-09-19 18:41:06 +0300

    column function

--- stage_2/analyzer/src/lib.rs
+++ stage_2/analyzer/src/lib.rs
@@ -14,6 +14,17 @@ pub fn read_csv(path: &str, delimiter: char) -> Result<Reader<File>, Box<dyn std
     Ok(csv_file)
 }
 
+pub fn print_csv(mut data: Reader<File>) -> Result<(), Box<dyn std::error::Error>> {
+    let my_records = data.records();
+
+    for i in my_records {
+        println!("{:?}", i);
+    }
+
+    Ok(())
+}
+
+
 pub fn print_columns(mut data: Reader<File>) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut myheaders = data.headers()?;
@@ -22,5 +33,20 @@ pub fn print_columns(mut data: Reader<File>) -> Result<(), Box<dyn std::error::E
         let column_name = column;
         println!("{}", column_name);
     }
+    Ok(())
+}
+
+pub fn calculate_column_stats(mut data: Reader<File>, column_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut myheaders = data.headers()?;
+
+
+    let index = myheaders.iter()
+        .enumerate()
+        .find(|(_, col)| *col == column_name)
+        .map(|(i, _)| i).unwrap();
+
+    println!("{:?}", index);
+
+    
     Ok(())
 }
--- stage_2/analyzer/src/main.rs
+++ stage_2/analyzer/src/main.rs
@@ -1,6 +1,8 @@
 use analyzer::{
     read_csv,
-    print_columns
+    print_columns,
+    print_csv,
+    calculate_column_stats,
 };
 use clap::Parser;
 
@@ -16,20 +18,57 @@ struct Cli {
     command: Option<Commands>
 }
 
+#[derive(Parser, Debug)]
 enum Commands {
     Print {
         #[clap(long, short)]
         path: String,
+        #[clap(long, short, default_value=",")]
         delimiter: String,
+    },
+    Headers {
+        #[clap(long, short)]
+        path: String,
+        #[clap(long, short, default_value=",")]
+        delimiter: String,
+    },
+    Stats {
+        #[clap(long, short)]
+        path: String,
+        #[clap(long, short, default_value=",")]
+        delimiter: String,
+        #[clap(long, short, default_value="id")]
+        column: String,
     }
 }
 
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> { 
-    let args = Args::parse();
+    let args = Cli::parse();
     
+    match args.command {
+        Some(Commands::Print { path, delimiter }) => {
+            let my_delimiter = delimiter.chars().next().unwrap();
+            let my_data = read_csv(&path, my_delimiter)?; 
+            print_csv(my_data);
+        }
+        Some(Commands::Headers { path, delimiter }) => {
+            let my_delimiter = delimiter.chars().next().unwrap();
+            let my_data = read_csv(&path, my_delimiter)?;
+            print_columns(my_data);
+        }
+        Some(Commands::Stats { path, delimiter, column }) => {
+            let my_delimiter = delimiter.chars().next().unwrap();
+            let my_data = read_csv(&path, my_delimiter)?;
+            let my_column = column.as_str();
 
+            calculate_column_stats(my_data, my_column);
+        }
+        _ =>  {
+            println!("No arguments passed");
+        }
+    };
+        
 
     Ok(())
 }
